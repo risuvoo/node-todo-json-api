b96c99d82cad75c2510f5e42ca4fe295c6b6fc46
risuvoo <rafiqulislamsuvobd@gmail.com>
2024-09-06 23:23:25 +0600

    ✨ Hello from express.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("hello from express.js");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("listening on port 3000");
+});
