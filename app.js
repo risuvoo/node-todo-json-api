f035639c561c548965193f8d511529e0fe249657
risuvoo <rafiqulislamsuvobd@gmail.com>
2024-09-06 23:57:26 +0600

    ✨ Get request for students

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const fs = require("fs");
 const app = express();
 app.get("/", (req, res) => {
   res.send("hello from express.js");
 });
-app.get("/another", (req, res) => {
-  res.send("another response");
-});
 
+// get request
+app.get("/api/students", (req, res) => {
+  fs.readFile("./db.json", "utf-8", (err, data) => {
+    const students = JSON.parse(data);
+    res.send(students);
+  });
+});
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
